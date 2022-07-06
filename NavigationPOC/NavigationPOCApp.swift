14e8db8876495291c3927c43cbebb3dd5a2c3949
Dino Constantinou <dconstantinou360@gmail.com>
2022-07-06 14:24:53 +0100

    Initial Commit

---
+++ NavigationPOC/NavigationPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationPOCApp.swift
+//  NavigationPOC
+//
+//  Created by Dino Constantinou on 06/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct NavigationPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
